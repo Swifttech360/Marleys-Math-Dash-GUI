d3a2e160cb50ef06c5bfcceda6119eebffd10f32
Swifttech360 <swifttech360@gmail.com>
2025-02-12 20:59:53 -0500

    Basic Button Creation

--- Marly's Math Dash.py
+++ Marly's Math Dash.py
@@ -1,29 +1,24 @@
 import tkinter as tk
-from tkinter import PhotoImage
-import time
-switchColor = lambda: screen.config(background='red')
-switchColor2 = lambda: screen.config(background='tomato')
-switchColor3 = lambda: screen.config(background='orange')
-switchColor4 = lambda: screen.config(background='gold')
-switchColor5 = lambda: screen.config(background='yellow')
-switchColor6 = lambda: screen.config(background='chartreuse')
-switchColor7 = lambda: screen.config(background='green')
-switchColor8 = lambda: screen.config(background='teal')
-switchColor9 = lambda: screen.config(background='cyan')
-switchColor10 = lambda: screen.config(background='lightBlue')
-switchColor11 = lambda: screen.config(background='blue')
-switchColor12 = lambda: screen.config(background='indigo')
-switchColor13 = lambda: screen.config(background='violet')
-switchColor14 = lambda: screen.config(background='magenta')
-switchColor15= lambda: screen.config(background='crimson')
+from tkinter import PhotoImage, Button
+def playButtonClick():
+    for i in range(5):
+        print("Joe Biden?")
+x = 0
+#screen1 creation
+screen1 = tk.Tk()
+screen1.config(background='red')
+screen1.geometry("889x500")
+screen1.title("Marly's Math Dash")
+windowIcon = PhotoImage(file = 'sprites/chillFish.png')
+screen1.iconphoto(True, windowIcon)
+
+#screen buttons
+playButton = Button(screen1, text = '     Play▶️', width=5, height=1  )
+playButton.pack()
+playButton.config(command = playButtonClick,
+                  font=('papyrus', 12, 'italic'),
+                  bg='#003187')
 
 
-screen = tk.Tk()
-screen.geometry("889x500")
-screen.title("Marly's Math Dash")
-windowIcon = PhotoImage(file= 'sprites/chillFish.png')
-screen.iconphoto(True, windowIcon)
-screen.config(background="red")
 
-
-screen.mainloop()
+screen1.mainloop()
