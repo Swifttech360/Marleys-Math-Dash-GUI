8ad8257d66f475474bd104b3dcd5a381576bc9b9
Swifttech360 <71281505+Swifttech360@users.noreply.github.com>
2025-02-23 00:12:11 -0500

    Proper Start Button
    - Cropped a real image for the start button
    
    - Placed the start button in the correct part of the screen

--- Marly's Math Dash.py
+++ Marly's Math Dash.py
@@ -2,22 +2,34 @@ import tkinter as tk
 from tkinter import PhotoImage, Button
 def playButtonClick():
     for i in range(5):
-        print("Joe Biden?")
+        print("You Pressed The Button")
 x = 0
 #screen1 creation
 screen1 = tk.Tk()
 screen1.config(background='red')
-screen1.geometry("889x500")
+screen1.geometry(f"{screen1.winfo_screenwidth()}x{screen1.winfo_screenheight()}")
 screen1.title("Marly's Math Dash")
 windowIcon = PhotoImage(file = 'sprites/chillFish.png')
 screen1.iconphoto(True, windowIcon)
 
 #screen buttons
-playButton = Button(screen1, text = '     Play▶️', width=5, height=1  )
+playSprite = PhotoImage(file='sprites/PerfectlyCroppedPlayButton.png')
+playSprite = playSprite.subsample(4, 4)
+playButton = Button(screen1)
 playButton.pack()
-playButton.config(command = playButtonClick,
-                  font=('papyrus', 12, 'italic'),
-                  bg='#003187')
+
+playButton.config(
+    command = playButtonClick,
+    image=playSprite,
+    borderwidth=0,
+    relief='flat',
+    bg='red',
+    activebackground='red'
+    )
+# NOTE: PlaySprite's dimensions are 1916x814
+#playButton.place(x=(2560-450) // 2,y= 500)
+playButton.place(relx=0.495, rely=0.68, anchor='center')
+
 
 
 
